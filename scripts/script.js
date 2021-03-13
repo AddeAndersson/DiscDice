bd34d88faf746fa25937012c23672cb6d7a4637a
Adrian Andersson <Adrian Andersson>
2021-03-13 17:27:15 +0100

    Added disc- and throw type

--- scripts/script.js
+++ scripts/script.js
@@ -1,26 +1,33 @@
 function roll() {
-      document.getElementById("scaler").className = "scale";
-      document.getElementById("spinner").className = "spin";
-      setTimeout(() => { document.getElementById("scaler").classList.remove("scale"); }, 500);
-      setTimeout(() => { document.getElementById("spinner").classList.remove("spin"); }, 500);
-  		var discType = [
-  			"overstable",
-  			"stable",
-  			"understable",
-  			"putter",
-  			"mid-range",
-  			"fairway driver",
-  			"distance driver",
-  			"thrower's choice"
-		  ];
-  		var throwType = [
-  			"hyzer",
-  			"roller",
-  			"anhyzer",
-  			"tomahawk",
-  			"forehand",
-  			"thrower's choice"
-		  ];
-  		document.getElementById("disc").innerHTML = "Disc: " + discType[Math.floor(Math.random() * discType.length)];
-  		document.getElementById("throw").innerHTML = "Throw: " + throwType[Math.floor(Math.random() * throwType.length)];
+  // Animation
+  document.getElementById("scaler").className = "scale";
+  document.getElementById("spinner").className = "spin";
+  setTimeout(() => { document.getElementById("scaler").classList.remove("scale"); }, 500);
+  setTimeout(() => { document.getElementById("spinner").classList.remove("spin"); }, 500);
+	
+  // Disc- and throw type
+  var discType = [
+		"overstable",
+		"stable",
+		"understable",
+		"putter",
+		"mid-range",
+		"fairway driver",
+		"distance driver",
+		"thrower's choice",
+    "opponent's choice"
+  ];
+	var throwType = [
+		"hyzer",
+		"roller",
+		"anhyzer",
+		"tomahawk",
+		"forehand",
+		"thrower's choice",
+    "opponent's choice"
+  ];
+
+  // Randomly select types
+	document.getElementById("disc").innerHTML = "Disc: " + discType[Math.floor(Math.random() * discType.length)];
+	document.getElementById("throw").innerHTML = "Throw: " + throwType[Math.floor(Math.random() * throwType.length)];
 }
