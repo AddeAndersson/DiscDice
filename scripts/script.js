ada888046cc1edb22631f868daf90e8b6d2061ad
Adrian Andersson <Adrian Andersson>
2021-03-27 11:56:40 +0100

    Added selectable types and discs before game starts

--- scripts/script.js
+++ scripts/script.js
@@ -1,33 +1,34 @@
+var enabledDiscs = [];
+var enabledThrows = [];
+
 function roll() {
   // Animation
   document.getElementById("scaler").className = "scale";
   document.getElementById("spinner").className = "spin";
   setTimeout(() => { document.getElementById("scaler").classList.remove("scale"); }, 500);
   setTimeout(() => { document.getElementById("spinner").classList.remove("spin"); }, 500);
-	
-  // Disc- and throw type
-  var discType = [
-		"overstable",
-		"stable",
-		"understable",
-		"putter",
-		"mid-range",
-		"fairway driver",
-		"distance driver",
-		"thrower's choice",
-    "opponent's choice"
-  ];
-	var throwType = [
-		"hyzer",
-		"roller",
-		"anhyzer",
-		"tomahawk",
-		"forehand",
-		"thrower's choice",
-    "opponent's choice"
-  ];
 
   // Randomly select types
-	document.getElementById("disc").innerHTML = "Disc: " + discType[Math.floor(Math.random() * discType.length)];
-	document.getElementById("throw").innerHTML = "Throw: " + throwType[Math.floor(Math.random() * throwType.length)];
+  document.getElementById("disc").innerHTML = "Disc: " + enabledDiscs[Math.floor(Math.random() * enabledDiscs.length)];
+  document.getElementById("throw").innerHTML = "Throw: " + enabledThrows[Math.floor(Math.random() * enabledThrows.length)];
+}
+
+function play() {
+
+	document.getElementById("discForm").style.display = "none";
+	document.getElementById("dice").style.display = "block";
+
+	var discs = document.getElementsByClassName("disc");
+	for(i = 0; i < discs.length; ++i) {
+		if(discs[i].checked) {
+			enabledDiscs.push(discs[i].value);
+		}
+	}
+
+	var types = document.getElementsByClassName("type");
+	for(i = 0; i < types.length; ++i) {
+		if(types[i].checked) {
+			enabledThrows.push(types[i].value);
+		}
+	}
 }
